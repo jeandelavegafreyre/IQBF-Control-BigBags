b1702952521ae46d4fc3f85b6e059b3c1ce8073f
jeandelavegafreyre <jean.de.la.vega.freyre@apmterminals.com>
2026-08-20 18:37:50 -0500

    Update User.cs

--- backend/IQBF.Domain/Entities/User.cs
+++ backend/IQBF.Domain/Entities/User.cs
@@ -1,1 +1,21 @@
+using IQBF.Domain.Common;
+using IQBF.Domain.Enums;
 
+namespace IQBF.Domain.Entities;
+
+public class User : BaseEntity
+{
+    public string UID { get; set; } = string.Empty;
+
+    public string FirstName { get; set; } = string.Empty;
+
+    public string LastName { get; set; } = string.Empty;
+
+    public string PasswordHash { get; set; } = string.Empty;
+
+    public UserRole Role { get; set; }
+
+    public bool IsActive { get; set; } = true;
+
+    public string FullName => $"{FirstName} {LastName}";
+}
