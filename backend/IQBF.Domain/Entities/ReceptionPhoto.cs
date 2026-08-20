d6b87a7f50da28997115c6b6109df07ca0fc7e47
jeandelavegafreyre <jean.de.la.vega.freyre@apmterminals.com>
2026-08-20 18:52:15 -0500

    Add ReceptionPhoto entity with properties

--- backend/IQBF.Domain/Entities/ReceptionPhoto.cs
+++ backend/IQBF.Domain/Entities/ReceptionPhoto.cs
@@ -1,1 +1,19 @@
+using IQBF.Domain.Common;
 
+namespace IQBF.Domain.Entities;
+
+public class ReceptionPhoto : BaseEntity
+{
+    public Guid ReceptionId { get; set; }
+
+    public string PhotoUrl { get; set; } = string.Empty;
+
+    public string? FileName { get; set; }
+
+    public string? ContentType { get; set; }
+
+    public long? FileSize { get; set; }
+
+    // Navigation Property
+    public Reception Reception { get; set; } = null!;
+}
