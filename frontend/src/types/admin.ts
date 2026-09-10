bcb3ace054eb388bf9de386422640a79642911f3
jeandelavegafreyre <jean.de.la.vega.freyre@apmterminals.com>
2026-09-09 23:46:13 -0500

    Add administration module

---
+++ frontend/src/types/admin.ts
@@ -0,0 +1,2 @@
+export interface Product { id: string; name: string; isActive: boolean }
+export interface UserSummary { id: string; uid: string; firstName: string; lastName: string; fullName: string; role: string | number; isActive: boolean }
