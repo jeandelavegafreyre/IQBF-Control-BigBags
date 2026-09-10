d015342aa5b4000ff112a842e4e1a081bca31d78
jeandelavegafreyre <jean.de.la.vega.freyre@apmterminals.com>
2026-09-10 01:16:54 -0500

    Include role in administrator user registration

--- backend/IQBF.Application/DTOs/Users/RegisterUserRequest.cs
+++ backend/IQBF.Application/DTOs/Users/RegisterUserRequest.cs
@@ -1,2 +1,10 @@
+using IQBF.Domain.Enums;
+
 namespace IQBF.Application.DTOs.Users;
-public sealed record RegisterUserRequest(string UID, string FirstName, string LastName, string Password);
+
+public sealed record RegisterUserRequest(
+    string UID,
+    string FirstName,
+    string LastName,
+    string Password,
+    UserRole Role = UserRole.User);
