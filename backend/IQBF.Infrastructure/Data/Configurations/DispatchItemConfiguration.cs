161be4297dce371b8a0b2769c77f9f2e5bf3dc55
jeandelavegafreyre <jean.de.la.vega.freyre@apmterminals.com>
2026-09-10 00:03:05 -0500

    Store dispatch quantities as integers

--- backend/IQBF.Infrastructure/Data/Configurations/DispatchItemConfiguration.cs
+++ backend/IQBF.Infrastructure/Data/Configurations/DispatchItemConfiguration.cs
@@ -8,27 +8,13 @@ public class DispatchItemConfiguration : IEntityTypeConfiguration<DispatchItem>
 {
     public void Configure(EntityTypeBuilder<DispatchItem> builder)
     {
-        builder.ToTable("DispatchItems", table =>
-        {
-            table.HasCheckConstraint("CK_DispatchItems_Quantity_Positive", "[Quantity] > 0");
-        });
-
+        builder.ToTable("DispatchItems", table => table.HasCheckConstraint("CK_DispatchItems_Quantity_Positive", "[Quantity] > 0"));
         builder.HasKey(x => x.Id);
-
-        builder.Property(x => x.Quantity).HasPrecision(18, 3).IsRequired();
+        builder.Property(x => x.Quantity).IsRequired();
         builder.HasIndex(x => new { x.DispatchId, x.BLId }).IsUnique();
-
         builder.Property(x => x.CreatedBy).HasMaxLength(50);
         builder.Property(x => x.UpdatedBy).HasMaxLength(50);
-
-        builder.HasOne(x => x.Dispatch)
-            .WithMany(x => x.Items)
-            .HasForeignKey(x => x.DispatchId)
-            .OnDelete(DeleteBehavior.Cascade);
-
-        builder.HasOne(x => x.BL)
-            .WithMany(x => x.DispatchItems)
-            .HasForeignKey(x => x.BLId)
-            .OnDelete(DeleteBehavior.Restrict);
+        builder.HasOne(x => x.Dispatch).WithMany(x => x.Items).HasForeignKey(x => x.DispatchId).OnDelete(DeleteBehavior.Cascade);
+        builder.HasOne(x => x.BL).WithMany(x => x.DispatchItems).HasForeignKey(x => x.BLId).OnDelete(DeleteBehavior.Restrict);
     }
 }
