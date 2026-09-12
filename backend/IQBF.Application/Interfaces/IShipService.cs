b92e1f8a570fc47faa43f3c6f7cc181e928c3863
jeandelavegafreyre <jean.de.la.vega.freyre@apmterminals.com>
2026-09-12 16:44:20 -0500

    Extend ship service for administration

--- backend/IQBF.Application/Interfaces/IShipService.cs
+++ backend/IQBF.Application/Interfaces/IShipService.cs
@@ -2,6 +2,8 @@ using IQBF.Application.DTOs.Ships;
 namespace IQBF.Application.Interfaces;
 public interface IShipService
 {
+    Task<IReadOnlyCollection<ShipDto>> GetAllAsync(CancellationToken cancellationToken = default);
     Task<IReadOnlyCollection<ShipDto>> GetActiveAsync(CancellationToken cancellationToken = default);
     Task<ShipDto> CreateAsync(CreateShipRequest request, string actorUid, CancellationToken cancellationToken = default);
+    Task<ShipDto> UpdateStatusAsync(Guid shipId, UpdateShipStatusRequest request, string actorUid, CancellationToken cancellationToken = default);
 }
