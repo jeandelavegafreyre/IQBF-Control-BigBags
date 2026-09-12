7be0d0dba85531500af3355da7a3edc6d9fe08dd
jeandelavegafreyre <jean.de.la.vega.freyre@apmterminals.com>
2026-09-12 15:22:16 -0500

    Add dispatch history and edit operations

--- backend/IQBF.Application/Interfaces/IDispatchService.cs
+++ backend/IQBF.Application/Interfaces/IDispatchService.cs
@@ -3,4 +3,6 @@ namespace IQBF.Application.Interfaces;
 public interface IDispatchService
 {
     Task<DispatchDto> CreateAsync(CreateDispatchRequest request, string actorUid, CancellationToken cancellationToken = default);
+    Task<IReadOnlyCollection<DispatchDto>> GetByShiftAsync(Guid shiftId, CancellationToken cancellationToken = default);
+    Task<DispatchDto> UpdateAsync(Guid id, CreateDispatchRequest request, string actorUid, CancellationToken cancellationToken = default);
 }
